392c33dc906c3a49248f85994b3e4db7f50c8e5b
Carlos Cuesta <carloscuesta@me.com>
2015-12-20 17:52:37 +0100

    Add ghost blog
    Add ghost blog.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 'use strict';
 
 var app = require('./src/app/carloscuesta.js'),
+	ghost = require('ghost'),
+	path = require('path'),
 	port = process.env.PORT || 5000;
 
-app.listen(port, function () {
-  console.log('carloscuesta @ '+port);
+ghost({
+	config: path.join(__dirname, '/src/app/ghost/config.js')
+}).then(function (ghostServer) {
+	app.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
+	ghostServer.start(app);
 });
+
+if (!module.parent) {
+	app.listen(port, function () {
+  		console.log('carloscuesta @ '+port);
+	});
+}
