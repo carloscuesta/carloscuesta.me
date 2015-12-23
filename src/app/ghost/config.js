67598f1fce48b3d5b6cd9ba1f30d2319ee4a8720
Carlos Cuesta <carloscuesta@me.com>
2015-12-23 16:18:56 +0100

    Update ghost config and styles
    Add mail configuration.

--- src/app/ghost/config.js
+++ src/app/ghost/config.js
@@ -1,8 +1,3 @@
-// # Ghost Configuration
-// Setup your Ghost install for various [environments](http://support.ghost.org/config/#about-environments).
-
-// Ghost runs in `development` mode by default. Full documentation can be found at http://support.ghost.org/config/
-
 var path = require('path'),
     config;
 
@@ -11,12 +6,20 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 config = {
-    // ### Production
-    // When running Ghost in the wild, use the production environment.
-    // Configure your URL and mail settings here
     production: {
         url: 'http://carloscuesta.me/blog',
-        mail: {},
+
+        mail: {
+    		transport: 'SMTP',
+    		options: {
+    		    service: 'Gmail',
+    		    auth: {
+    		        user: process.env.EMAIL,
+    		        pass: process.env.EMAIL_PASSWORD
+    		    }
+    		}
+		},
+
         database: {
             client: 'pg',
             connection: {
@@ -36,29 +39,20 @@ config = {
         }
     },
 
-    // ### Development **(default)**
     development: {
-        // The url to use when providing links to the site, E.g. in RSS and email.
-        // Change this to your Ghost blog's published URL.
-        url: 'http://carloscuesta.me/blog',
+        url: 'http://localhost:5000/blog',
 
-        // Example mail config
-        // Visit http://support.ghost.org/mail for instructions
-        // ```
-        //  mail: {
-        //      transport: 'SMTP',
-        //      options: {
-        //          service: 'Mailgun',
-        //          auth: {
-        //              user: '', // mailgun username
-        //              pass: ''  // mailgun password
-        //          }
-        //      }
-        //  },
-        // ```
+        mail: {
+    		transport: 'SMTP',
+    		options: {
+    		    service: 'Gmail',
+    		    auth: {
+    		        user: process.env.EMAIL,
+    		        pass: process.env.EMAIL_PASSWORD
+    		    }
+    		}
+		},
 
-        // #### Database
-        // Ghost supports sqlite3 (default), MySQL & PostgreSQL
         database: {
             client: 'pg',
             connection: {
@@ -71,26 +65,17 @@ config = {
             },
             debug: false
         },
-        // #### Server
-        // Can be host & port (default), or socket
+
         server: {
-            // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
-            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
             port: '2368'
         },
-        // #### Paths
-        // Specify where your content directory lives
+
         paths: {
             contentPath: path.join(__dirname, '/content/')
         }
     },
 
-    // **Developers only need to edit below here**
-
-    // ### Testing
-    // Used when developing Ghost to run tests and check the health of Ghost
-    // Uses a different port number
     testing: {
         url: 'http://127.0.0.1:2369',
         database: {
@@ -106,8 +91,6 @@ config = {
         logging: false
     },
 
-    // ### Testing MySQL
-    // Used by Travis - Automated testing run through GitHub
     'testing-mysql': {
         url: 'http://127.0.0.1:2369',
         database: {
@@ -127,8 +110,6 @@ config = {
         logging: false
     },
 
-    // ### Testing pg
-    // Used by Travis - Automated testing run through GitHub
     'testing-pg': {
         url: 'http://127.0.0.1:2369',
         database: {
