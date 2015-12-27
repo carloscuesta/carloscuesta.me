26bb94c51d7a14f3718e992c8e2405f6900b3145
Carlos Cuesta <carloscuesta@me.com>
2015-12-27 14:33:48 +0100

    Update twitterapiclient
    Remove and delete access tokens from the file and twitter api
    application.

--- src/app/scripts/twitterapiclient.js
+++ src/app/scripts/twitterapiclient.js
@@ -1,22 +1,24 @@
 'use strict';
 
+require('dotenv').load();
+
 var ApiClient = require('./apiclient'),
 	CacheApiClient = require('./cache');
 
 var TwitterApiClient = (function() {
 
 	var _ApiClient = new ApiClient({
-        base_url: 'https://api.twitter.com/1.1',
-        oauth: {
-            request_token: 'https://api.twitter.com/oauth/request_token',
-            access_token: 'https://api.twitter.com/oauth/access_token',
-            consumer_key: 'vpF1dfWXDHiFaMT7LTFl0TOSq',
-            consumer_secret: 'HwQWzgRDXe4FBKiQXpcoqyOgJhDuo9z4KQqdY8przFNlVgsQcB',
-            access_token: '2974751872-3D4Bb0YFIJBadjAWZ3IP0vgPxOnEUilSiExbWqM',
-            access_token_secret: 'LLMzB5Itdv6i4VaqPVmMmXi2rZXmDxN3v1SDw0bDHwXnl',
-        },
-        debug: true
-    });
+		base_url: 'https://api.twitter.com/1.1',
+		oauth: {
+			request_token: 'https://api.twitter.com/oauth/request_token',
+			access_token: 'https://api.twitter.com/oauth/access_token',
+			consumer_key: process.env.TWITTER_CONSUMER_KEY,
+			consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+			access_token: process.env.TWITTER_ACCES_TOKEN,
+			access_token_secret: process.env.TWITTER_ACCES_TOKEN_SECRET,
+		},
+		debug: true
+	});
 
 	var getUserTimeline = function(params) {
 		return CacheApiClient.validate.call(_ApiClient,'/statuses/user_timeline.json', params);
