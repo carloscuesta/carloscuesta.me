23369093885d41e41284eb463bf937150e10da40
Carlos Cuesta <carloscuesta@me.com>
2015-12-29 20:14:54 +0100

    Update routes, templates add ghost tpl
    Update routes, twitter api fetch excludes replies and rts, ghost blog
    only takes 2 posts and includes tags. Added ghost templates to show at
    the main page (needs more jade and css) #2

--- src/app/routes.js
+++ src/app/routes.js
@@ -23,12 +23,16 @@ exports.index = function(req, res) {
 
 	var userTimeline = TwitterApiClient.getUserTimeline({
 		screen_name: 'crloscuesta',
-		count: 4
+		count: 4,
+		exclude_replies: true,
+		include_rts: false
 	});
 
 	var lastPosts = GhostApiClient.getLastPosts({
 		client_id: process.env.GHOST_CLIENT_ID,
-		client_secret: process.env.GHOST_CLIENT_SECRET
+		client_secret: process.env.GHOST_CLIENT_SECRET,
+		limit: 2,
+		include: 'tags'
 	});
 
 	Promise.all([ghUserCCStars, userTimeline, lastPosts]).then(function(data) {
@@ -36,6 +40,7 @@ exports.index = function(req, res) {
 		res.render('views/index', {
 			githubData: data[0],
 			twitterData: tweets,
+			ghostData: data[2],
 			me: staticData.me,
 			site: staticData.site,
 			social: staticData.social,
