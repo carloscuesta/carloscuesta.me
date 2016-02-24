6cbf8f4e15f8f25eb9dc85fa4c5658c5dea74d79
Carlos Cuesta <carloscuesta@me.com>
2016-02-24 18:40:53 +0100

    Update routes.js
    Set count:5

--- src/app/routes.js
+++ src/app/routes.js
@@ -23,7 +23,7 @@ exports.index = function(req, res) {
 
 	var userTimeline = TwitterApiClient.getUserTimeline({
 		screen_name: 'crloscuesta',
-		count: 4,
+		count: 5,
 		exclude_replies: false,
 		include_rts: true
 	});
