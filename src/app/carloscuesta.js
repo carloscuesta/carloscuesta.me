8f95598104bf3e3b2ece1144ceda359d9ce17b6e
Carlos Cuesta <carloscuesta@me.com>
2016-03-01 16:05:59 +0100

    Update routes
    Serve js static

--- src/app/carloscuesta.js
+++ src/app/carloscuesta.js
@@ -9,6 +9,7 @@ var express = require('express'),
 carloscuesta.use(compression());
 carloscuesta.use(express.static(__dirname+'/static/'));
 carloscuesta.use(express.static(__dirname+'/static/img'));
+carloscuesta.use(express.static(__dirname+'/static/js'));
 carloscuesta.set('views', __dirname + '/templates');
 carloscuesta.set('view engine', 'jade');
 carloscuesta.engine('jade', require('jade').__express);
