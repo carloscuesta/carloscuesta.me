909719c651bbd17ac37d6095c08f30b927944450
Carlos Cuesta <carloscuesta@me.com>
2016-03-25 11:11:05 +0100

    Update carloscuesta.js
    Browser caching

--- src/app/carloscuesta.js
+++ src/app/carloscuesta.js
@@ -7,18 +7,21 @@ var express = require('express'),
     compression = require('compression');
 
 carloscuesta.use(compression());
-carloscuesta.use(express.static(__dirname+'/static/'));
-carloscuesta.use(express.static(__dirname+'/static/img'));
-carloscuesta.use(express.static(__dirname+'/static/js'));
+carloscuesta.use(function (req, res, next) {
+    res.setHeader('Cache-Control', 'public, max-age=86400');
+    next();
+});
+carloscuesta.use(express.static(__dirname+'/static/img', {maxAge: 86400000}));
+carloscuesta.use(express.static(__dirname+'/static/js', {maxAge: 86400000}));
+carloscuesta.use(express.static(__dirname + '/static/js', {maxAge: 86400000}));
 carloscuesta.set('views', __dirname + '/templates');
 carloscuesta.set('view engine', 'jade');
-carloscuesta.engine('jade', require('jade').__express);
 carloscuesta.use(sassMiddleware({
     src: __dirname+'/styles',
     dest: __dirname+'/static/css',
     outputStyle: 'compressed'
 }));
-
+carloscuesta.use(express.static(__dirname+'/static/css', {maxAge: 86400000}));
 
 carloscuesta.get('/', routes.index);
 carloscuesta.get('/'+process.env.PARAM_CLEAN, routes.cacheClean);
