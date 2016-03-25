ef2fdd1a4a88fca30a95dabe18e411b543962373
Carlos Cuesta <carloscuesta@me.com>
2016-03-25 17:02:52 +0100

    Update package, carloscuesta and routes
    Set jade cache to true, removed serve-static

--- src/app/carloscuesta.js
+++ src/app/carloscuesta.js
@@ -3,13 +3,10 @@
 var express = require('express'),
     carloscuesta = express(),
     sassMiddleware = require('node-sass-middleware'),
-    serveStatic = require('serve-static'),
     routes = require('./routes'),
     compression = require('compression');
 
 carloscuesta.use(compression());
-carloscuesta.use(serveStatic(__dirname + '/static/img', {maxAge: '1d',}));
-carloscuesta.use(serveStatic(__dirname + '/static/js', {maxAge: '1d',}));
 carloscuesta.set('views', __dirname + '/templates');
 carloscuesta.set('view engine', 'jade');
 carloscuesta.use(sassMiddleware({
@@ -17,7 +14,10 @@ carloscuesta.use(sassMiddleware({
     dest: __dirname+'/static/css',
     outputStyle: 'compressed'
 }));
-carloscuesta.use(serveStatic(__dirname + '/static/css', {maxAge: '1d',}));
+
+carloscuesta.use(express.static('/static/img', {maxage: '86400000'}));
+carloscuesta.use(express.static('/static/js', {maxage: '86400000'}));
+carloscuesta.use(express.static('/static/css', {maxage: '86400000'}));
 
 carloscuesta.get('/', routes.index);
 carloscuesta.get('/'+process.env.PARAM_CLEAN, routes.cacheClean);
--- src/app/routes.js
+++ src/app/routes.js
@@ -48,7 +48,7 @@ exports.index = function(req, res) {
 			me: staticData.me,
 			site: staticData.site,
 			social: staticData.social,
-			cache: false
+			cache: true
 		});
 	});
 };
