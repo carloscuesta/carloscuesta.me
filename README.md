fe88f55a52b33d884d8544273ed9140bcd0d5c25
Carlos Cuesta <carloscuesta@me.com>
2016-03-28 12:55:58 +0200

    Add more tests, update aboutme template and style, update package.json
    Add tests to check the cache-control header, refactored tests. Update
    readme with the coveralls badge, updated styles and templates for
    aboutme to separate the styles from the index view. Add coverage/
    folder to .gitignore. Removed x2 test at the package.json

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 [![Build Status](https://img.shields.io/travis/carloscuesta/carloscuesta.me.svg?style=flat-square)](https://travis-ci.org/carloscuesta/carloscuesta.me)
 [![Code Climate](https://img.shields.io/codeclimate/github/carloscuesta/carloscuesta.me.svg?style=flat-square)](https://codeclimate.com/github/carloscuesta/carloscuesta.me/)
+[![Coverage](https://img.shields.io/coveralls/carloscuesta/carloscuesta.me.svg?style=flat-square)](https://coveralls.io/github/carloscuesta/carloscuesta.me)
 [![Dependencies](https://img.shields.io/david/carloscuesta/carloscuesta.me.svg?style=flat-square)](https://david-dm.org/carloscuesta/carloscuesta.me)
 [![DevDependencies](https://img.shields.io/david/dev/carloscuesta/carloscuesta.me.svg?style=flat-square)](https://david-dm.org/carloscuesta/carloscuesta.me#info=devDependencies)
 [![Heroku](https://img.shields.io/badge/deployed-heroku-430098.svg?style=flat-square)](http://carloscuesta.me)
--- test/app.js
+++ test/app.js
@@ -4,52 +4,80 @@ var request = require('supertest'),
 	should = require('should'),
 	app = require ('../src/app/carloscuesta'),
 	cache = require('memory-cache'),
-	port = process.env.PORT || 5000;
+	port = process.env.PORT || 3000;
 
 describe('app', function() {
  	before(function () {
         app.listen(port);
   	});
 
-	describe('status', function() {
+	describe('views', function() {
 
-		it('should return 200 Ok', function(done) {
-			request(app)
-				.get('/')
-				.end(function(err, res) {
-					res.status.should.equal(200);
-					done();
+		describe('index', function() {
+
+			it('should render and return 200 OK ', function(done) {
+				request(app)
+					.get('/')
+					.end(function(err, res) {
+						res.status.should.equal(200);
+						done();
+					});
+			});
+
+			describe('caching', function() {
+
+				it('should return the cache-control header with max-age 1 day', function(done) {
+					request(app)
+						.get('/')
+						.end(function(err, res) {
+							res.headers['cache-control'].should.be.equal('public, max-age=86400');
+							done();
+						});
 				});
+
+				it('should cache API JSON responses', function(done) {
+					request(app)
+						.get('/')
+						.end(function(err, res) {
+							cache.size().should.be.aboveOrEqual(1);
+							done();
+						});
+				});
+
+				it('should clear the cache', function(done) {
+					request(app)
+						.get('/'+process.env.PARAM_CLEAN)
+						.end(function(err, res) {
+							cache.size().should.be.belowOrEqual(0);
+							done();
+						});
+				});
+			});
 		});
 
-		it('should return 404 Not found', function(done) {
-			request(app)
-				.get('/404')
-				.end(function(err, res) {
-					res.status.should.equal(404);
-					done();
-				});
-		});
-	});
+		describe('aboutme', function() {
 
-	describe('caching', function() {
+			it('should render and return 200 OK ', function(done) {
+				request(app)
+					.get('/aboutme')
+					.end(function(err, res) {
+						res.status.should.equal(200);
+						done();
+					});
+			});
 
-		it('should cache API JSON responses', function(done) {
-			request(app)
-				.get('/')
-				.end(function(err, res) {
-					cache.size().should.be.aboveOrEqual(1);
-					done();
-				});
-		});
+			describe('caching', function() {
 
-		it('should clear the cache', function(done) {
-			request(app)
-				.get('/'+process.env.PARAM_CLEAN)
-				.end(function(err, res) {
-					cache.size().should.be.belowOrEqual(0);
-					done();
+				it('should return the cache-control header with max-age 1 day', function(done) {
+					request(app)
+						.get('/')
+						.end(function(err, res) {
+							res.headers['cache-control'].should.be.equal('public, max-age=86400');
+							done();
+						});
 				});
+			});
+
 		});
 	});
 });
