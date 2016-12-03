e50d2669c68384cf165fb9182cc1c03efeecdab9
Maxime Le Conte des Floris <maxime.lcdf@gmail.com>
2016-12-03 12:41:30 +0100

    Fix typo

--- src/app/data/staticdata.js
+++ src/app/data/staticdata.js
@@ -7,7 +7,7 @@ var staticData = {
 		job: 'Front End Developer',
 		company: 'Mediàtic',
 		hometown: 'Badalona',
-		aboutme: 'Front End developer based in Barcelona, Spain. Working at <a href="http://mediatic.cat" target="_blank" onmousedown="ga(\'send\', \'event\', \'Link\', \'Company\');",>Mediàtic</a>. Coding addicted that enjoys building things with code, I like design, tecnhology and I <span class="heart">&#9829;</span> OpenSource.',
+		aboutme: 'Front End developer based in Barcelona, Spain. Working at <a href="http://mediatic.cat" target="_blank" onmousedown="ga(\'send\', \'event\', \'Link\', \'Company\');",>Mediàtic</a>. Coding addicted that enjoys building things with code, I like design, technology and I <span class="heart">&#9829;</span> OpenSource.',
 		mail: 'hi@carloscuesta.me',
 		avatar: 'carloscuesta.jpeg',
 		jobs: [
@@ -103,7 +103,7 @@ var staticData = {
 
 	site: {
 		title: 'Carlos Cuesta | Front End Developer',
-		description: 'Carlos Cuesta, Front End developer based in Barcelona, Spain. Coding addicted that enjoys building things with code, I like design, tecnhology and OpenSource.',
+		description: 'Carlos Cuesta, Front End developer based in Barcelona, Spain. Coding addicted that enjoys building things with code, I like design, technology and OpenSource.',
 		pages: {
 			home: 'https://carloscuesta.me',
 			blog: 'https://carloscuesta.me/blog',
