d97968814a900d9f5d8672a4b9afbc1bae1f2ca1
Carlos Cuesta <carloscuesta@me.com>
2017-03-03 21:47:00 +0100

    :wrench: Refactor express app

--- src/app/carloscuesta.js
+++ src/app/carloscuesta.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var express = require('express'),
-    carloscuesta = express(),
-    sassMiddleware = require('node-sass-middleware'),
-    routes = require('./routes'),
-    compression = require('compression');
+const express = require('express');
+const carloscuesta = express();
+const sassMiddleware = require('node-sass-middleware');
+const routes = require('./routes');
+const compression = require('compression');
 
 carloscuesta.use(compression());
 
-carloscuesta.set('views', __dirname + '/templates');
+carloscuesta.set('views', `${__dirname}/templates`);
 carloscuesta.set('view engine', 'pug');
 carloscuesta.set('view cache', true);
 
@@ -18,17 +18,17 @@ carloscuesta.use(sassMiddleware({
     outputStyle: 'compressed'
 }));
 
-carloscuesta.use(express.static(__dirname +  '/static/'));
-carloscuesta.use(express.static(__dirname +  '/static/css/'));
-carloscuesta.use(express.static(__dirname +  '/static/js/'));
+carloscuesta.use(express.static(`${__dirname}/static/`));
+carloscuesta.use(express.static(`${__dirname}/static/css/`));
+carloscuesta.use(express.static(`${__dirname}/static/js/`));
 
 carloscuesta.get('/', routes.index);
 carloscuesta.get('/about', routes.about);
-carloscuesta.get('/'+process.env.PARAM_CLEAN, routes.cacheClean);
+carloscuesta.get(`/${process.env.PARAM_CLEAN}`, routes.cacheClean);
 
-carloscuesta.use(function(error, req, res, next) {
+carloscuesta.use((error, req, res, next) => {
     res.status(500);
-    res.render('views/errors/500', {error: error});
+    res.render('views/errors/500', { error: error });
 });
 
 module.exports = carloscuesta;
