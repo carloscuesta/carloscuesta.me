a6710cb3065bfa91d9bd68e1ad5984fbb6173345
Carlos Cuesta <carloscuesta@me.com>
2017-03-03 20:46:10 +0100

    :hammer: Refactor TwitterApiClient

--- src/app/scripts/twitterapiclient.js
+++ src/app/scripts/twitterapiclient.js
@@ -2,58 +2,52 @@
 
 require('dotenv').load();
 
-var ApiClient = require('./apiclient'),
-	twitterParse = require('twitter-text'),
-	moment = require('moment'),
-	CacheApiClient = require('./cache'),
-	nodeCache = require('memory-cache');
+const ApiClient = require('./apiclient');
+const twitterParse = require('twitter-text');
+const moment = require('moment');
+const CacheApiClient = require('./cache');
+const nodeCache = require('memory-cache');
 
-var TwitterApiClient = (function() {
+const CACHE_TIME = 86400000
+const dataCache = nodeCache.get('parsedTweets')
 
-	var _ApiClient = new ApiClient({
-		base_url: 'https://api.twitter.com/1.1',
-		oauth: {
-			request_token: 'https://api.twitter.com/oauth/request_token',
-			access_token: 'https://api.twitter.com/oauth/access_token',
-			consumer_key: process.env.TWITTER_CONSUMER_KEY,
-			consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-			access_token: process.env.TWITTER_ACCES_TOKEN,
-			access_token_secret: process.env.TWITTER_ACCES_TOKEN_SECRET,
-		},
-		debug: false
-	});
+class TwitterApiClient {
+	constructor() {
+		this.apiClient = new ApiClient({
+			base_url: 'https://api.twitter.com/1.1',
+			oauth: {
+				request_token: 'https://api.twitter.com/oauth/request_token',
+				access_token: 'https://api.twitter.com/oauth/access_token',
+				consumer_key: process.env.TWITTER_CONSUMER_KEY,
+				consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+				access_token: process.env.TWITTER_ACCES_TOKEN,
+				access_token_secret: process.env.TWITTER_ACCES_TOKEN_SECRET,
+			},
+			debug: false
+		})
+	}
 
-	var getUserTimeline = function(params) {
-		return CacheApiClient.validate.call(_ApiClient,'/statuses/user_timeline.json', params);
-	};
+	getUserTimeline(params) {
+		return CacheApiClient.validate
+			.call(this.apiClient, '/statuses/user_timeline.json', params)
+	}
 
-	var parseTweets = function(tweets) {
-		var _cacheTime =  86400000,
-			_dataCache = nodeCache.get('parsedTweets');
-
-		if (!_dataCache) {
-			var twits = tweets.map(function(tweet){
-				var media = tweet.entities.media;
-				var parsedText = twitterParse.autoLink(tweet.text);
+	parseTweets(tweets) {
+		if (!dataCache) {
+			const tweetMap = tweets.map((tweet) => {
+				const media = tweet.entities.media;
+				const parsedText = twitterParse.autoLink(tweet.text);
 
 				return Object.assign({
 					image: (media && media.length > 0) ? media[0].media_url_https : '',
 					text_parsed: parsedText
-				}, tweet);
-			});
-
-			nodeCache.put('parsedTweets', twits, _cacheTime);
-			return twits;
-		} else {
-			return _dataCache;
+				}, tweet)
+			})
+			nodeCache.put('parsedTweets', tweetMap, CACHE_TIME);
+			return tweetMap;
 		}
-	};
+		return dataCache;
+	}
+}
 
-	return {
-		getUserTimeline: getUserTimeline,
-		parseTweets: parseTweets
-	};
-
-})();
-
-module.exports = TwitterApiClient;
+module.exports = new TwitterApiClient();
