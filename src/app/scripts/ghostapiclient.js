2f87c8530fe61ffb3740785347ba828e70b09776
Carlos Cuesta <carloscuesta@me.com>
2017-03-03 20:49:53 +0100

    :hammer: Refactor GhostApiClient

--- src/app/scripts/ghostapiclient.js
+++ src/app/scripts/ghostapiclient.js
@@ -1,52 +1,48 @@
 'use strict';
 
-var ApiClient = require('./apiclient'),
-	CacheApiClient = require('./cache'),
-	nodeCache = require('memory-cache'),
-	moment = require('moment'),
-	striptags = require('striptags');
+const ApiClient = require('./apiclient');
+const CacheApiClient = require('./cache');
+const nodeCache = require('memory-cache');
+const moment = require('moment');
+const stripTags = require('striptags');
 
-var GhostApiClient = (function() {
+const CACHE_TIME = 86400000;
+const dataCache = nodeCache.get('parsedPosts')
 
-	var _ApiClient = new ApiClient({
-		'base_url': 'http://carloscuesta.me/blog/ghost/api/v0.1',
-		'cache': 5
-	});
+class GhostApiClient {
+	constructor() {
+		this.apiClient = new ApiClient({
+			'base_url': 'http://carloscuesta.me/blog/ghost/api/v0.1',
+			'cache': 5
+		});
+	}
 
-	var getLastPosts = function(params) {
-		return CacheApiClient.validate.call(_ApiClient,'/posts', params);
-	};
+	getLastPosts(params) {
+		return CacheApiClient.validate
+			.call(this.apiClient, '/posts', params)
+	}
 
-	var parsePosts = function(postData) {
-		var _cacheTime =  86400000,
-			_dataCache = nodeCache.get('parsedPosts');
-
+	parsePosts(post) {
 		Array.prototype.insert = function (index, item) {
 			this.splice(index, 0, item);
 		};
 
-		if (!_dataCache) {
-			for (var i = 0; i < postData.posts.length; i++) {
-				var image = postData.posts[i].image.split('/'),
-					params = 'w_500';
-					image.insert(6, params);
-				var parsedImages = image.join('/');
+		if (!dataCache) {
+			for (var i = 0; i < post.posts.length; i++) {
+				const params = 'w_500';
+				const image = post.posts[i].image.split('/');
+				image.insert(6, params);
+				const parsedImages = image.join('/');
 
-				postData.posts[i].image = parsedImages;
-				postData.posts[i].published_at = moment(postData.posts[i].updated_at).startOf('hour').fromNow();
-				postData.posts[i].html = striptags(postData.posts[i].html).substring(0,120)+' ...';
+				post.posts[i].image = parsedImages;
+				post.posts[i].published_at = moment(post.posts[i].updated_at).startOf('hour').fromNow();
+				post.posts[i].html = stripTags(post.posts[i].html).substring(0,120)+' ...';
 			}
-			nodeCache.put('parsedPosts', postData, _cacheTime);
-			return postData;
-		} else {
-			return _dataCache;
+			nodeCache.put('parsedPosts', post, CACHE_TIME);
+			return post;
 		}
-	};
+		return dataCache;
+	}
+}
 
-	return {
-		getLastPosts: getLastPosts,
-		parsePosts: parsePosts
-	};
-})();
-
-module.exports = GhostApiClient;
+module.exports = new GhostApiClient();
