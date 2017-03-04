f585d26ffd82104a166860d6739408c6bee1084e
Carlos Cuesta <carloscuesta@me.com>
2017-03-04 11:19:20 +0100

    :hammer: Apply codestyle in app.js

--- src/app/carloscuesta.js
+++ src/app/carloscuesta.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const express = require('express');
-const carloscuesta = express();
 const sassMiddleware = require('node-sass-middleware');
-const routes = require('./routes');
 const compression = require('compression');
+const routes = require('./routes');
+
+const carloscuesta = express();
 
 carloscuesta.use(compression());
 
@@ -13,9 +14,9 @@ carloscuesta.set('view engine', 'pug');
 carloscuesta.set('view cache', true);
 
 carloscuesta.use(sassMiddleware({
-    src: __dirname+'/styles',
-    dest: __dirname+'/static/css',
-    outputStyle: 'compressed'
+	src: `${__dirname}/styles`,
+	dest: `${__dirname}/static/css`,
+	outputStyle: 'compressed'
 }));
 
 carloscuesta.use(express.static(`${__dirname}/static/`));
@@ -26,9 +27,9 @@ carloscuesta.get('/', routes.index);
 carloscuesta.get('/about', routes.about);
 carloscuesta.get(`/${process.env.PARAM_CLEAN}`, routes.cacheClean);
 
-carloscuesta.use((error, req, res, next) => {
-    res.status(500);
-    res.render('views/errors/500', { error: error });
+carloscuesta.use((error, req, res) => {
+	res.status(500);
+	res.render('views/errors/500', {error: error});
 });
 
 module.exports = carloscuesta;
