970dd4b1a51c31e754f100062da846a02aa9ae9d
Carlos Cuesta <carloscuesta@me.com>
2017-09-29 20:08:57 +0200

    :fire: Remove Ghost from app entry point

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 const app = require('./src/app/carloscuesta')
-const ghost = require('ghost')
 const port = process.env.PORT || 5000
 
-ghost({ config: `${__dirname}/src/blog/config.js` }).then((ghostServer) => {
-  app.use(ghostServer.config.paths.subdir, ghostServer.rootApp)
-  app.use((req, res) => {
-    res.status(404)
-    res.render('errors/404')
-  })
-  ghostServer.start(app)
+app.listen(port, () => {
+  console.log(`carloscuesta @ ${port}`)
 })
-
-if (!module.parent) {
-  app.listen(port, () => {
-    console.log(`carloscuesta @ ${port}`)
-  })
-}
