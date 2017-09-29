c5e8c759a4bcf1799fd905295fb781e0a1e59ad3
Carlos Cuesta <carloscuesta@me.com>
2017-09-29 21:44:32 +0200

    :truck: Rename main entry point

--- carloscuesta.js
+++ carloscuesta.js
@@ -1,6 +1,4 @@
 const app = require('./src/app/carloscuesta')
 const port = process.env.PORT || 5000
 
-app.listen(port, () => {
-  console.log(`carloscuesta @ ${port}`)
-})
+app.listen(port, () => console.log(`carloscuesta @ ${port}`))
