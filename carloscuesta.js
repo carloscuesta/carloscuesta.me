247999a3f520427a12dc66e1336e540c7f42bd68
Carlos Cuesta <carloscuesta@me.com>
2017-09-29 22:55:43 +0200

    :truck: Move app content to src/

--- carloscuesta.js
+++ carloscuesta.js
@@ -1,4 +1,4 @@
-const app = require('./src/app/carloscuesta')
+const app = require('./src/carloscuesta')
 const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`carloscuesta @ ${port}`))
