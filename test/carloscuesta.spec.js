fa9710084b4af5d98846d49dc5cabba2418decc8
Carlos Cuesta <carloscuesta@me.com>
2017-10-08 20:50:17 +0200

    :sparkles: Switch to Jest and add Standard (#56)
    * :heavy_plus_sign: Add Standard
    
    * :art: Standard
    
    * :heavy_plus_sign: Add Jest
    
    * :hammer: Refactor tests to use jest
    
    * :wrench: Update .travis.yml and package.json scripts
    
    * :heavy_minus_sign: Remove old test devDependencies
    
    * :construction_worker: Set Travis email notifications to false
    
    * :white_check_mark: Set jest.timeout

---
+++ test/carloscuesta.spec.js
@@ -0,0 +1,51 @@
+/* global jest, describe, expect, it */
+const cache = require('memory-cache')
+const request = require('supertest')
+
+const app = require('../src/carloscuesta')
+
+jest.setTimeout(10000)
+
+describe('carloscuesta.me', () => {
+  describe('views', () => {
+    describe('index', () => {
+      it('should render home and set cache', (done) => {
+        request(app).get('/').end((err, res) => {
+          if (err) {
+            return console.error(err)
+          }
+          expect(res.status).toEqual(200)
+          expect(cache.size()).toBeGreaterThan(1)
+          expect(res.headers['cache-control']).toMatchSnapshot()
+          done()
+        })
+      })
+    })
+
+    describe('aboutme', () => {
+      it('should render about and set cache', (done) => {
+        request(app).get('/about').end((err, res) => {
+          if (err) {
+            return console.error(err)
+          }
+          expect(res.status).toEqual(200)
+          expect(res.headers['cache-control']).toMatchSnapshot()
+          done()
+        })
+      })
+    })
+  })
+
+  describe('cache', () => {
+    it('should clear the cache', (done) => {
+      request(app).get(`/${process.env.PARAM_CLEAN}`).end((err, res) => {
+        if (err) {
+          return console.error(err)
+        }
+        expect(res.headers['cache-control']).toBeUndefined()
+        expect(cache.size()).toBe(0)
+        done()
+      })
+    })
+  })
+})
