5ee1409a935600c1292ab33c98cda6e2d7fda048
Carlos Cuesta <carloscuesta@me.com>
2017-10-08 19:06:13 +0200

    :art: Standard

--- gruntfile.js
+++ gruntfile.js
@@ -1,31 +1,28 @@
-'use strict';
+module.exports = function (grunt) {
+  grunt.loadNpmTasks('grunt-pagespeed')
 
-module.exports = function(grunt) {
-
-    grunt.loadNpmTasks('grunt-pagespeed');
-
-    grunt.initConfig({
-        pagespeed: {
-            options: {
-                nokey: true,
-                url: 'https://carloscuesta.me',
-                locale: 'en_GB',
-                threshold: 90
-            },
-            desktop: {
-                options: {
-                    strategy: 'desktop'
-                }
-            },
-            mobile: {
-                options: {
-                    strategy: 'mobile'
-                }
-            }
+  grunt.initConfig({
+    pagespeed: {
+      options: {
+        nokey: true,
+        url: 'https://carloscuesta.me',
+        locale: 'en_GB',
+        threshold: 90
+      },
+      desktop: {
+        options: {
+          strategy: 'desktop'
         }
-    });
+      },
+      mobile: {
+        options: {
+          strategy: 'mobile'
+        }
+      }
+    }
+  })
 
-    grunt.registerTask('default', function() {
-        grunt.task.run('pagespeed');
-    });
-};
+  grunt.registerTask('default', function () {
+    grunt.task.run('pagespeed')
+  })
+}
