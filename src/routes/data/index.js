1ca00c595d33b7e7d5ee431f3c4e2bb69bcf818b
Carlos Cuesta <carloscuesta@me.com>
2017-12-17 13:04:03 +0100

    📝 Update info (#58)
    * :memo: Update data
    
    * :construction_worker: Update travis.yml
    
    * :wrench: Update node engine version

--- src/routes/data/index.js
+++ src/routes/data/index.js
@@ -3,8 +3,8 @@
 const moment = require('moment')
 
 const me = {
-  aboutme: 'Front End developer based in Barcelona, Spain. Working at <strong>Ulabox</strong>. Coding addicted that enjoys building things with code, I like design, technology and I ❤️ OpenSource.',
-  age: 21,
+  aboutme: 'Front End engineer based in Barcelona, Spain. Working at <strong>Ulabox</strong>. Coding addicted that enjoys building things with code, I like design, technology and I ❤️ OpenSource.',
+  age: 22,
   avatar: 'carloscuesta.jpeg',
   companyName: 'Ulabox',
   countriesVisited: [
