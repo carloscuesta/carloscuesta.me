fe141d894b66a751e77fce4383b8ec33c4b58137
Carlos Cuesta <carloscuesta@me.com>
2020-06-01 13:09:43 +0200

    :sparkles: Add Biography component

---
+++ src/pages/index/index.js
@@ -0,0 +1,18 @@
+// @flow
+import React from 'react'
+
+import Header from './components/Header'
+import Biography from './components/Biography'
+
+type Props = {}
+
+const Index = (props: Props) => (
+  <>
+    <Header />
+    <main>
+      <Biography />
+    </main>
+  </>
+)
+
+export default Index
