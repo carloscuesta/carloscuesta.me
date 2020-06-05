3a9aa85683bf5e8c38e3eb8cfc1ae3349a2108f8
Carlos Cuesta <carloscuesta@me.com>
2020-06-05 22:53:29 +0200

    :sparkles: Add OpenSource component

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,13 +2,17 @@
 import React from 'react'
 
 import { fetchPosts } from 'src/utils/api/ghost'
+import { fetchRepositories } from 'src/utils/api/github'
 import { type Post } from 'src/utils/api/ghost/mutators'
+import { type Repository } from 'src/utils/github/mutators'
 import Header from './components/Header'
 import Biography from './components/Biography'
 import Writings from './components/Writings'
+import OpenSource from './components/OpenSource'
 
 type Props = {
-  posts: Array<Post>
+  posts: Array<Post>,
+  repositories: Array<Repository>
 }
 
 const Index = (props: Props) => (
@@ -17,18 +21,21 @@ const Index = (props: Props) => (
     <main>
       <Biography />
       <Writings posts={props.posts} />
+      <OpenSource repositories={props.repositories} />
     </main>
   </>
 )
 
 export const getServerSideProps = async () => {
-  const [posts] = await Promise.all([
-    fetchPosts()
+  const [posts, repositories] = await Promise.all([
+    fetchPosts(),
+    fetchRepositories()
   ])
 
   return {
     props: {
-      posts
+      posts,
+      repositories
     }
   }
 }
