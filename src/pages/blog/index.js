112137007778fc2c6371121d0599c98a8c14c021
Carlos Cuesta <carloscuesta@me.com>
2020-06-14 17:30:44 +0200

    :sparkles: Add blog page

---
+++ src/pages/blog/index.js
@@ -0,0 +1,39 @@
+// @flow
+import React from 'react'
+
+import { fetchPosts } from 'src/utils/api/blog'
+import { type Post } from 'src/utils/api/blog/mutators'
+import NavigationMenu from 'src/components/shared/NavigationMenu'
+import BlogPost from 'src/components/shared/BlogPost'
+import Wrapper from 'src/components/shared/Wrapper'
+
+type Props = {
+  posts: Array<Post>
+}
+
+const Blog = (props: Props) => (
+  <>
+    <NavigationMenu />
+    <main>
+      <Wrapper>
+        <div className='row'>
+          {props.posts.map((post) => <BlogPost post={post} key={post.slug} />)}
+        </div>
+      </Wrapper>
+    </main>
+  </>
+)
+
+export const getStaticProps = async () => {
+  const [posts] = await Promise.all([
+    fetchPosts()
+  ])
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
+export default Blog
