7dd43f8a74afa4c1c40d99d5dedb66061d46ba93
Carlos Cuesta <carloscuesta@me.com>
2020-06-22 12:00:13 +0200

    :sparkles: Render Posts grouped by year at Writings

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,5 +1,7 @@
 // @flow
 import React from 'react'
+import groupBy from 'lodash.groupby'
+import { NextSeo } from 'next-seo'
 
 import { fetchPosts } from 'src/utils/api/blog'
 import { type Post } from 'src/utils/api/blog/mutators'
@@ -7,29 +9,42 @@ import BlogPost from 'src/components/shared/BlogPost'
 import Wrapper from 'src/components/shared/Wrapper'
 
 type Props = {
-  posts: Array<Post>
+  posts: { [key: string]: Array<Post> }
 }
 
 const Blog = (props: Props) => (
   <>
+    <NextSeo
+      canonical='https://carloscuesta.me/blog'
+      title='Carlos Cuesta – Front End Engineer – Blog'
+    />
     <main>
       <Wrapper>
-        <div className='row'>
-          {props.posts.map((post) => <BlogPost post={post} key={post.slug} />)}
-        </div>
+        {Object.keys(props.posts).sort(() => -1).map((year: string) => (
+          <div className='row' key={year}>
+            <div className='col-xs-12'>
+              <h3>{year}</h3>
+            </div>
+
+            {props.posts[year].map((post) => (
+              <BlogPost post={post} key={post.slug} />
+            ))}
+          </div>
+        ))}
       </Wrapper>
     </main>
   </>
 )
 
 export const getStaticProps = async (): Promise<{ props: Props }> => {
-  const [posts] = await Promise.all([
-    fetchPosts()
-  ])
+  const posts: Array<Post> = await fetchPosts()
 
   return {
     props: {
-      posts
+      posts: groupBy(
+        posts,
+        (post) => new Date(post.datePublished.value).getFullYear()
+      )
     }
   }
 }
