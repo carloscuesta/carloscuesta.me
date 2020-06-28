e0971f021e9a5b7eee870b400e847218bb14c2cb
Carlos Cuesta <carloscuesta@me.com>
2020-06-28 18:52:16 +0200

    :wrench: Setup Lighthouse CI (#187)

---
+++ .lighthouserc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: 'npm run start',
+      startServerReadyPattern: 'started server on',
+      url: [
+        'http://localhost:3000',
+        'http://localhost:3000/blog'
+      ],
+      settings: {
+        skipAudits: [
+          'is-on-https',
+          'canonical',
+          'uses-http2',
+          'uses-rel-preconnect',
+          'uses-rel-preload'
+        ]
+      }
+    },
+    upload: {
+      target: 'temporary-public-storage'
+    }
+  }
+}
