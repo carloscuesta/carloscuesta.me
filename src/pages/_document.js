5bab2de040cbbca46c73ef995618e190d36bca32
Carlos Cuesta <carloscuesta@me.com>
2020-06-28 17:14:07 +0200

    :wrench: Update manifest.json file (#188)

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -27,7 +27,10 @@ class Document extends NextDocument {
           <link rel='apple-touch-icon' sizes='72x72' href='/favicon/apple-icon-72x72.png' />
           <link rel='apple-touch-icon' sizes='76x76' href='/favicon/apple-icon-76x76.png' />
           <link rel='icon' type='image/png' sizes='16x16' href='/favicon/favicon-16x16.png' />
-          <link rel='icon' type='image/png' sizes='192x192' href='/favicon/android-icon-192x192.png' />
+          <link
+            rel='icon' type='image/png' sizes='192x192' href='/favicon/
+          icon-192x192.png'
+          />
           <link rel='icon' type='image/png' sizes='32x32' href='/favicon/favicon-32x32.png' />
           <link rel='icon' type='image/png' sizes='96x96' href='/favicon/favicon-96x96.png' />
           <link rel='manifest' href='/manifest.json' />
