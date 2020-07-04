3b2eeab186ff5d375c71977346dde2ccf0749818
Carlos Cuesta <carloscuesta@me.com>
2020-07-04 12:24:00 +0200

    :sparkles: Add about page

---
+++ src/pages/about/index.js
@@ -0,0 +1,28 @@
+// @flow
+import React from 'react'
+import { NextSeo, SocialProfileJsonLd } from 'next-seo'
+
+const About = () => (
+  <>
+    <NextSeo
+      canonical='https://carloscuesta.me/about'
+      title='Carlos Cuesta – About me'
+    />
+    <SocialProfileJsonLd
+      type='Person'
+      name='Carlos Cuesta'
+      url='https://carloscuesta.me'
+      sameAs={[
+        'http://instagram.com/crloscuesta',
+        'https://github.com/carloscuesta',
+        'https://twitter.com/crloscuesta',
+        'https://www.linkedin.com/in/crloscuesta'
+      ]}
+    />
+    <main>
+
+    </main>
+  </>
+)
+
+export default About
