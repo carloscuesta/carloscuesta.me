282ce9e6d4b9638844caf19ce0e600639ae5a29b
Carlos Cuesta <carloscuesta@me.com>
2020-07-28 12:10:56 +0200

    :white_check_mark: Mock next/Link
    Related with https://github.com/vercel/next.js/issues/15543

--- src/components/shared/Layout/__tests__/layout.spec.js
+++ src/components/shared/Layout/__tests__/layout.spec.js
@@ -19,6 +19,8 @@ Router.useRouter = () => ({
   }
 })
 
+jest.mock('next/link', () => 'a')
+
 describe('Layout', () => {
   it('should render the Layout with children', () => {
     const wrapper = renderer.create(
