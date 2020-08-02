36c906902877386d853ee1685d0a0d62219b5476
Carlos Cuesta <carloscuesta@me.com>
2020-08-02 15:47:33 +0200

    :bug: Fix window.ga is not a function

--- src/utils/analytics/index.js
+++ src/utils/analytics/index.js
@@ -1,11 +1,17 @@
 // @flow
 export const GA_TRACKING_ID: string = 'UA-67824860-3'
 
+const isFunction = (fn) => typeof fn === 'function'
+
 export const trackPageView = (url: string) => {
-  window.ga('set', 'page', url)
-  window.ga('send', 'pageview')
+  if (isFunction(window.ga)) {
+    window.ga('set', 'page', url)
+    window.ga('send', 'pageview')
+  }
 }
 
 export const trackEvent = (event: Object) => {
-  window.ga('send', 'event', event)
+  if (isFunction(window.ga)) {
+    window.ga('send', 'event', event)
+  }
 }
