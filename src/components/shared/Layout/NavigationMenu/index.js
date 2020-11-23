91f21d8f9195213c5e981b13f4010c40c9bdc80b
Carlos Cuesta <carloscuesta@me.com>
2020-11-23 22:04:08 +0100

    :sparkles: Add avatar to NavigationMenu Logo

--- src/components/shared/Layout/NavigationMenu/index.js
+++ src/components/shared/Layout/NavigationMenu/index.js
@@ -1,5 +1,6 @@
 // @flow
 import React, { type Element } from 'react'
+import Link from 'next/link'
 
 import Wrapper from 'src/components/shared/Wrapper'
 import MenuLink from './MenuLink'
@@ -10,9 +11,19 @@ const NavigationMenu = (): Element<'header'> => (
   <header className={styles.header}>
     <Wrapper>
       <nav className={styles.navigation}>
-        <strong className={styles.logo}>
-          <MenuLink href='/' text='Carlos Cuesta' />
-        </strong>
+        <Link href='/'>
+          <a className={styles.logo}>
+            <img
+              alt='Carlos Cuesta'
+              className={styles.avatar}
+              height={36}
+              src='https://res.cloudinary.com/carloscuesta/image/upload/s--g0fD72tH--/c_scale,q_100,w_72/v1594588508/carloscuesta.jpg'
+              width={36}
+            />
+
+            Carlos Cuesta
+          </a>
+        </Link>
 
         <ul className={styles.links}>
           <li><MenuLink href='/blog' text='Blog' /></li>
