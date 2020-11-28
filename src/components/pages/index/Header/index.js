f8091a5c4bf22b510799f5d239fbd3c4f21b172a
Carlos Cuesta <carloscuesta@me.com>
2020-11-28 17:20:24 +0100

    :sparkles: Improve site header
    :pencil2: Fix typo on DefaultSeo

--- src/components/pages/index/Header/index.js
+++ src/components/pages/index/Header/index.js
@@ -1,14 +1,20 @@
 // @flow
 import React, { type Element } from 'react'
+import Link from 'next/link'
 
+import Wrapper from 'src/components/shared/Wrapper'
 import styles from './styles.module.css'
 
 const Header = (): Element<'header'> => (
-  <header className={styles.header}>
-    <span className={styles.headline}>Hi! I'm</span>
-    <h1 className={styles.name}>Carlos Cuesta</h1>
-    <span className={[styles.wave, styles.headline].join(' ')}>👋🏼</span>
-    <h2 className={styles.job}>Front End Engineer</h2>
+  <header className={styles.container}>
+    <Wrapper>
+      <h1 className={styles.title}>Hi, I'm <span className={styles.name}>Carlos Cuesta</span></h1>
+      <span className={styles.wave}>👋🏼</span>
+      <h2 className={styles.bio}>
+        I'm a <b>Front End Engineer</b> from Barcelona.
+        I <a target='_blank' rel='noopener noreferrer' href='https://github.com/carloscuesta'>code</a>, <Link href='/blog'><a>write</a></Link> and build stuff on internet.
+      </h2>
+    </Wrapper>
   </header>
 )
 
