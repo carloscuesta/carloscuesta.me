cca90b1a1664b5177783a400ed0e6bfbdfeb2239
Carlos Cuesta <carloscuesta@me.com>
2020-12-13 18:03:32 +0100

    💬 Update Biography text

--- src/components/pages/index/Biography/index.js
+++ src/components/pages/index/Biography/index.js
@@ -5,29 +5,36 @@ import Wrapper from 'src/components/shared/Wrapper'
 import styles from './styles.module.css'
 
 const Biography = (): Element<'section'> => (
-  <section className={styles.biography}>
+  <section>
     <Wrapper>
-      <div className='row middle-xs'>
+      <div className='row middle-xs between-xs'>
         <div className='col-xs-12 col-sm-6'>
-          <h3 className={styles.biographyText}>
-            I'm a 24 year old <b>Front End Engineer</b> based in <b>Barcelona</b>,
-            working at <a href='https://ulabox.com' rel='noopener noreferrer'><b>Ulabox</b></a>.
-            I'm a coding passionate focused on <b>JavaScript</b>, specifically React & React-Native.
-            Interested in design, tech and <b>OpenSource</b> ❤️
+          <h3 className={styles.text}>
+            I'm a 24–year–old Front End Engineer interested in
+            design, under engineering and open source.
+            I work remotely at <a href='https://ulabox.com' rel='noopener noreferrer'>Ulabox</a>.
           </h3>
 
-          <h3 className={styles.biographyText}>
+          <h3 className={styles.text}>
+            I'm focused on building products with JavaScript, specifically React and React-Native.
+          </h3>
+
+          <h3 className={styles.text}>
+            When I'm not coding, I'm likely at the gym or surfing the city with my longboard.
+          </h3>
+
+          <h3 className={styles.text}>
             You can find me on{' '}
             <a href='https://github.com/carloscuesta' target='_blank' rel='noopener noreferrer'>GitHub</a>,{' '}
             <a href='https://twitter.com/crloscuesta' target='_blank' rel='noopener noreferrer'>Twitter</a>{' and '}
-            <a href='https://linkedin.com/in/crloscuesta' target='_blank' rel='noopener noreferrer'>LinkedIn</a>
+            <a href='https://linkedin.com/in/crloscuesta' target='_blank' rel='noopener noreferrer'>LinkedIn</a>.
           </h3>
         </div>
 
         <div className='col-xs-12 col-sm-6'>
           <img
             alt='Carlos Cuesta'
-            className={`lazyload ${styles.profilePicture}`}
+            className={`lazyload ${styles.picture}`}
             data-src='https://res.cloudinary.com/carloscuesta/image/upload/s--bhEud7Xt--/q_80/v1594588508/carloscuesta.jpg'
             src='https://res.cloudinary.com/carloscuesta/image/upload/s--rq7ZUVpY--/c_scale,e_blur:100,h_120,q_10,w_120/v1594588508/carloscuesta.jpg'
           />
