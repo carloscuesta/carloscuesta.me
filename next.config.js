f914559ec1904c0635b0f2b61fc85a231c71bf7c
Carlos Cuesta <carloscuesta@me.com>
2022-08-29 15:19:17 +0200

    🔧 Update `next-pwa` config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-const withPWA = require('next-pwa')
-
-module.exports = withPWA({
-  pwa: {
-    disable: process.env.NODE_ENV === 'development',
-    dest: 'public'
-  }
+const withPWA = require('next-pwa')({
+  disable: process.env.NODE_ENV === 'development',
+  dest: 'public'
 })
+
+module.exports = withPWA({})
