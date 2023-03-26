31b347767102df5b63917991f80f7fdafa7e02df
Carlos Cuesta <carloscuesta@me.com>
2023-03-26 20:58:43 +0200

    ♻️ Tailwindify `PageTitle`

--- src/components/shared/PageTitle/index.tsx
+++ src/components/shared/PageTitle/index.tsx
@@ -1,11 +1,9 @@
-import styles from './styles.module.css'
-
 type Props = {
   title: string
 }
 
 const PageTitle = (props: Props) => (
-  <h1 className={styles.title}>
+  <h1 className='text-4xl font-bold my-6'>
     {props.title}
   </h1>
 )
