29e8ff019ef9286e6f0201d83c8b4d46d441a4f0
Carlos Cuesta <carloscuesta@me.com>
2023-03-31 23:07:53 +0200

    ♻️ Tailwindify `Wrapper`

--- src/components/shared/Wrapper/index.tsx
+++ src/components/shared/Wrapper/index.tsx
@@ -1,9 +1,10 @@
-import styles from './styles.module.css'
-
-type Props = { children: JSX.Element[] | JSX.Element, isCompressed?: boolean }
+type Props = { 
+  children: JSX.Element[] | JSX.Element, 
+  isCompressed?: boolean
+}
 
 const Wrapper = (props: Props) => (
-  <div className={props.isCompressed ? styles.wrapperCompressed : styles.wrapper}>
+  <div className={`m-[0_auto] px-6 sm:px-4 max-w-4xl ${props.isCompressed ? 'max-w-[800px]' : ''}`}>
     {props.children}
   </div>
 )
