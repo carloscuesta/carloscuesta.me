bf345a85f5818083d5a77ad11965abee8966eaaf
Carlos Cuesta <carloscuesta@me.com>
2023-04-17 22:07:48 +0200

    🚚 Migrate `projects` to app directory

--- src/app/projects/components/Repositories/index.tsx
+++ src/app/projects/components/Repositories/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useState } from 'react'
 
 import SectionTitle from 'src/components/shared/SectionTitle'
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,57 @@
+import { type Metadata } from 'next'
+
+import { fetchRepositories, fetchUserInformation } from 'src/utils/api/github'
+import { fetchPublishedPackages, fetchDownloadsCount } from 'src/utils/api/npm'
+import PageTitle from 'src/components/shared/PageTitle'
+import Wrapper from 'src/components/shared/Wrapper'
+import Repositories from './components/Repositories'
+import Stats from './components/Stats'
+
+export const metadata: Metadata = {
+  title: 'Uses',
+  alternates: {
+    canonical: '/uses',
+  },
+}
+
+const Projects = async () => {
+  const data = await getData()
+
+  return (
+    <main>
+      <Wrapper>
+        <PageTitle title="Projects" />
+
+        <Stats
+          followers={data.userInformation.followers}
+          packageDownloads={data.packageDownloads}
+          stars={data.stars}
+        />
+
+        <Repositories repositories={data.repositories} />
+      </Wrapper>
+    </main>
+  )
+}
+
+export const revalidate = 3600
+
+const getData = async () => {
+  const [repositories, userInformation, publishedPackages] = await Promise.all([
+    fetchRepositories(),
+    fetchUserInformation(),
+    fetchPublishedPackages(),
+  ])
+
+  return {
+    packageDownloads: await fetchDownloadsCount(publishedPackages),
+    repositories,
+    stars: repositories
+      .map((repository) => repository.stars.value)
+      .reduce((memo, value) => memo + value)
+      .toLocaleString(),
+    userInformation,
+  }
+}
+
+export default Projects
