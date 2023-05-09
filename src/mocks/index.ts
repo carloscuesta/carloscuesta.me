ec5d490d1ab38eb213a29444cc7ac4940e10422a
Carlos Cuesta <carloscuesta@me.com>
2023-05-09 20:27:50 +0200

    👨‍💻 Enable back msw

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -3,10 +3,10 @@ const IS_SERVER = typeof window === 'undefined'
 async function setupMocks() {
   if (IS_SERVER) {
     const { server } = await import('./server')
-    server.listen()
+    server.listen({ onUnhandledRequest: 'bypass' })
   } else {
     const { worker } = await import('./browser')
-    worker.start()
+    worker.start({ quiet: true })
   }
 }
 
