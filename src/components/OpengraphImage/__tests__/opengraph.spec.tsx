73c2a8df0571adaf43aef3e7d073e65fd1c9fe42
Carlos Cuesta <carloscuesta@me.com>
2024-02-11 13:28:16 +0100

    ✅ Add spec for `getFonts`

--- src/components/OpengraphImage/__tests__/opengraph.spec.tsx
+++ src/components/OpengraphImage/__tests__/opengraph.spec.tsx
@@ -1,6 +1,7 @@
 import renderer from 'react-test-renderer'
+import fetch, { enableFetchMocks } from 'jest-fetch-mock'
 
-import OpengraphImage from '..'
+import OpengraphImage, { getFonts } from '..'
 
 describe('OpengraphImage', () => {
   it('should match a page opengraph', () => {
@@ -26,5 +27,17 @@ describe('OpengraphImage', () => {
     )
 
     expect(wrapper).toMatchSnapshot()
+  })
+
+  describe('getFonts', () => {
+    beforeAll(() => {
+      enableFetchMocks()
+    })
+
+    it('should return an array of fonts', async () => {
+      const fonts = await getFonts()
+
+      expect(fonts).toMatchSnapshot()
+    })
   })
 })
