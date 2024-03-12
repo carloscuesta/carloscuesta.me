920ce789a31c1f1f0056eb6c5641166d6e1597d8
Carlos Cuesta <carloscuesta@me.com>
2024-03-13 00:28:15 +0100

    ⚡️ Add image placeholder

--- src/app/about/components/WhoAmI/Polaroid/index.tsx
+++ src/app/about/components/WhoAmI/Polaroid/index.tsx
@@ -17,7 +17,14 @@ const Polaroid = (props: Props) => (
     className={`select-none p-4 rounded-lg bg-gradient-to-b from-white to-gray-100 shadow-2xl dark:from-gray-200 dark:to-gray-200 text-center ${rotation[props.rotation]} hover:scale-105 hover:rotate-0 transition-transform ease-out`}
   >
     <div className="relative h-fit w-fit overflow-hidden rounded">
-      <Image width="240" height="240" src={props.src} alt={props.label} />
+      <Image
+        alt={props.label}
+        height="240"
+        loading="eager"
+        placeholder="data:image/gif;base64,R0lGODlhAQABAPAAANTU1P///yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
+        src={props.src}
+        width="240"
+      />
       <div className="absolute inset-0 shadow-[inset_0_0_4px_rgba(0,0,0,.3)]" />
     </div>
     <p className="mt-3 text-center text-gray-600 dark:text-gray-800 md:text-lg xl:text-xl">
