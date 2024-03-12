9c6f55d2a6bac86437f9de2822e8166b2ac707bd
Carlos Cuesta <carloscuesta@me.com>
2024-03-13 00:43:07 +0100

    ✨ Revamp `/about` picture section (#1455)
    * 🍱 Add polaroid font
    * ✨ Add `Polaroid` component
    * ✨ Replace image with `Polaroid` components
    * 🚚 Move images to folder
    * 💄 Add pt to photos
    * ✅ Update tests
    * ⚡️ Optimize photos
    * ⚡️ Add image placeholder
    * ✅ Update tests
    * 💄 Adjust colors and text size

---
+++ src/app/about/components/WhoAmI/Polaroid/index.tsx
@@ -0,0 +1,36 @@
+import Image, { type ImageProps } from 'next/image'
+
+const rotation = {
+  '-3': '-rotate-3',
+  '1': 'rotate-1',
+  '-2': '-rotate-2',
+} as const
+
+type Props = {
+  label: string
+  src: ImageProps['src']
+  rotation: keyof typeof rotation
+}
+
+const Polaroid = (props: Props) => (
+  <div
+    className={`select-none p-4 rounded-lg bg-gradient-to-b from-white to-neutral-100 shadow-2xl dark:from-neutral-200 dark:to-neutral-200 text-center ${rotation[props.rotation]} hover:scale-105 hover:rotate-0 transition-transform ease-out`}
+  >
+    <div className="relative h-fit w-fit overflow-hidden rounded">
+      <Image
+        alt={props.label}
+        height="240"
+        loading="eager"
+        placeholder="data:image/gif;base64,R0lGODlhAQABAPAAANTU1P///yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
+        src={props.src}
+        width="240"
+      />
+      <div className="absolute inset-0 shadow-[inset_0_0_4px_rgba(0,0,0,.3)]" />
+    </div>
+    <p className="mt-3 text-center text-neutral-600 dark:text-neutral-800 text-md">
+      {props.label}
+    </p>
+  </div>
+)
+
+export default Polaroid
