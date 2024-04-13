15f10c961536bfa3385382582dbf263ad2fe2d9c
Carlos Cuesta <carloscuesta@me.com>
2024-04-13 20:01:40 +0200

    🔒 Add headers to next.config

--- next.config.js
+++ next.config.js
@@ -5,27 +5,61 @@ const withPWA = require('next-pwa')({
   dest: 'public'
 })
 
-module.exports = withPWA(withPlaiceholder({
-  experimental: {
-    // https://beta.nextjs.org/docs/api-reference/next-config#servercomponentsexternalpackages
-    serverComponentsExternalPackages: ['uglify-js', 'plaiceholder']
-  },
-  images: {
-    remotePatterns: [
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    ${process.env.NODE_ENV === 'development' ? '' : 'upgrade-insecure-requests'};
+`
+
+module.exports = withPWA(
+  withPlaiceholder({
+    experimental: {
+      // https://beta.nextjs.org/docs/api-reference/next-config#servercomponentsexternalpackages
+      serverComponentsExternalPackages: ['uglify-js', 'plaiceholder'],
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'res.cloudinary.com',
+          port: '',
+          pathname: '/carloscuesta/image/upload/**',
+        },
+      ],
+      deviceSizes: [640, 768, 1024, 1280, 1536],
+    },
+    redirects: async () => [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/carloscuesta/image/upload/**'
-      }
+        source: '/opensource',
+        destination: '/projects',
+        permanent: true,
+      },
     ],
-    deviceSizes: [640, 768, 1024, 1280, 1536],
-  },
-  redirects: async () => [
-    {
-      source: '/opensource',
-      destination: '/projects',
-      permanent: true
-    }
-  ]
-}))
+    headers: async () => [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+        ],
+      },
+    ],
+  }),
+)
