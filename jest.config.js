5f5bfa2b27f72065d46b1b2de1a399722119f9b9
Carlos Cuesta <carloscuesta@me.com>
2024-10-22 18:24:51 +0200

    ✨ Add jest.setup file

--- jest.config.js
+++ jest.config.js
@@ -4,30 +4,27 @@ const createJestConfig = nextJest({ dir: './' })
 
 async function jestConfig() {
   const nextJestConfig = await createJestConfig({
-    "collectCoverageFrom": [
-      "src/**/*.{js,jsx,ts,tsx}",
-      "!src/**/__tests__/*.{js,jsx,ts,tsx}",
-      "!src/mocks/**",
+    collectCoverageFrom: [
+      'src/**/*.{js,jsx,ts,tsx}',
+      '!src/**/__tests__/*.{js,jsx,ts,tsx}',
+      '!src/mocks/**',
       // Ignoring these files as of https://github.com/vercel/next.js/issues/47299
-      "!src/**/**/layout.tsx",
-      "!src/**/**/route.ts",
-      "!src/**/**/page.tsx",
-      "!src/**/**/not-found.tsx",
-      "!src/**/**/providers.tsx",
-      "!src/**/**/opengraph-image.tsx",
+      '!src/**/**/layout.tsx',
+      '!src/**/**/route.ts',
+      '!src/**/**/page.tsx',
+      '!src/**/**/not-found.tsx',
+      '!src/**/**/providers.tsx',
+      '!src/**/**/opengraph-image.tsx',
     ],
-    "testMatch": [
-      "**/*.(spec).(js)",
-      "**/*.(spec).(ts)",
-      "**/*.(spec).(tsx)"
-    ],
-    "moduleNameMapper": {
-      "src/(.*)$": "<rootDir>/src/$1",
-      // For some reason Jest is not able to resolve `rehype-pretty-code` 
+    testMatch: ['**/*.(spec).(js)', '**/*.(spec).(ts)', '**/*.(spec).(tsx)'],
+    moduleNameMapper: {
+      'src/(.*)$': '<rootDir>/src/$1',
+      // For some reason Jest is not able to resolve `rehype-pretty-code`
       // when running tests
-      "rehype-pretty-code": "<rootDir>/node_modules/rehype-pretty-code",
+      'rehype-pretty-code': '<rootDir>/node_modules/rehype-pretty-code',
     },
-    "testEnvironment": "jsdom"
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   })()
 
   // Add ignores for specific ESM packages so they are transformed by Jest
