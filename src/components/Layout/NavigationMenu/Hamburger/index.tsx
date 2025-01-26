d0b7676f789bd24a6c07c760f5249a45ff2b3448
Carlos Cuesta <carloscuesta@me.com>
2025-01-26 23:52:58 +0100

    ♻️ Replace `body-scroll-lock`

--- src/components/Layout/NavigationMenu/Hamburger/index.tsx
+++ src/components/Layout/NavigationMenu/Hamburger/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { usePathname } from 'next/navigation'
-import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
+import { RemoveScroll } from 'react-remove-scroll'
 
 import Links from '../Links'
 import OpenIcon from './OpenIcon'
@@ -13,18 +13,6 @@ const Hamburger = (props: Props) => {
   const pathname = usePathname()
   const buttonClassName =
     'fill-black p-2 dark:fill-white focus:outline-hidden focus:ring-1 focus:ring-neutral-300 dark:focus:ring-neutral-500 rounded-lg'
-
-  useEffect(() => {
-    if (isOpen) {
-      const documentBody = document.body
-
-      disableBodyScroll(documentBody)
-
-      return () => {
-        enableBodyScroll(documentBody)
-      }
-    }
-  }, [isOpen])
 
   useEffect(() => {
     if (isOpen) {
@@ -44,29 +32,31 @@ const Hamburger = (props: Props) => {
       </button>
 
       {props.isOpen && (
-        <nav className="fixed bottom-0 left-0 right-0 top-0 z-10 bg-white dark:bg-black">
-          <div className="absolute right-0 px-6 py-2">
-            <button
-              aria-label="Close navigation menu"
-              onClick={() => setIsOpen(false)}
-              className={buttonClassName}
-              tabIndex={0}
-            >
-              <CloseIcon />
-            </button>
-          </div>
+        <RemoveScroll>
+          <nav className="fixed bottom-0 left-0 right-0 top-0 z-10 bg-white dark:bg-black">
+            <div className="absolute right-0 px-6 py-2">
+              <button
+                aria-label="Close navigation menu"
+                onClick={() => setIsOpen(false)}
+                className={buttonClassName}
+                tabIndex={0}
+              >
+                <CloseIcon />
+              </button>
+            </div>
 
-          <Links
-            isHamburguer
-            links={[
-              { href: '/', text: 'Home' },
-              { href: '/about', text: 'About' },
-              { href: '/blog', text: 'Blog' },
-              { href: '/uses', text: 'Uses' },
-              { href: '/photos', text: 'Photos' },
-            ]}
-          />
-        </nav>
+            <Links
+              isHamburguer
+              links={[
+                { href: '/', text: 'Home' },
+                { href: '/about', text: 'About' },
+                { href: '/blog', text: 'Blog' },
+                { href: '/uses', text: 'Uses' },
+                { href: '/photos', text: 'Photos' },
+              ]}
+            />
+          </nav>
+        </RemoveScroll>
       )}
     </div>
   )
