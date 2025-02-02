fd7d3eb7cf59209c970152423d0bf3efaf4536c6
Carlos Cuesta <carloscuesta@me.com>
2025-02-03 00:29:52 +0100

    🐛 Fix Tailwind opacity

--- src/app/blog/[slug]/components/NewsletterSubscribe/index.tsx
+++ src/app/blog/[slug]/components/NewsletterSubscribe/index.tsx
@@ -31,7 +31,7 @@ const NewsletterSubscribe = () => (
           placeholder="you@email.com"
           required
           type="email"
-          className="text-sm rounded-md border border-neutral-400 border-opacity-30 bg-transparent px-2 py-1 text-neutral-700 opacity-100 outline-hidden ring-neutral-300  transition-all hover:border-opacity-50 hover:shadow-xs focus:ring-2 dark:border-neutral-700 dark:text-neutral-200 dark:ring-neutral-500 dark:hover:border-neutral-600 dark:hover:text-white sm:space-x-2"
+          className="text-sm rounded-md border border-neutral-400/30  bg-transparent px-2 py-1 text-neutral-700 opacity-100 outline-hidden ring-neutral-300  transition-all hover:border-neutral-400/50 hover:shadow-xs focus:ring-2 dark:border-neutral-700 dark:text-neutral-200 dark:ring-neutral-500 dark:hover:border-neutral-600 dark:hover:text-white sm:space-x-2"
         />
 
         <input type="hidden" value="1" name="embed" />
@@ -39,7 +39,7 @@ const NewsletterSubscribe = () => (
         <input
           type="submit"
           value="Subscribe"
-          className="text-sm shadow-2xs cursor-pointer rounded-md border border-neutral-400 border-opacity-30 bg-transparent px-4 py-1 font-semibold text-neutral-700 outline-hidden ring-neutral-300 transition-all hover:border-opacity-50 hover:shadow-xs focus:ring-2 dark:border-neutral-700 dark:text-neutral-200 dark:ring-neutral-500 dark:hover:border-neutral-600 dark:hover:text-white"
+          className="text-sm shadow-2xs cursor-pointer rounded-md border border-neutral-400/30  bg-transparent px-4 py-1 font-semibold text-neutral-700 outline-hidden ring-neutral-300 transition-all hover:border-neutral-400/50 hover:shadow-xs focus:ring-2 dark:border-neutral-700 dark:text-neutral-200 dark:ring-neutral-500 dark:hover:border-neutral-600 dark:hover:text-white"
         />
       </div>
     </div>
--- src/app/blog/[slug]/components/ShareLinks/Button/index.tsx
+++ src/app/blog/[slug]/components/ShareLinks/Button/index.tsx
@@ -6,7 +6,7 @@ type Props = {
 const Button = (props: Props) => (
   <a
     href={props.href}
-    className="shadow-2xs text-sm flex flex-none cursor-pointer items-center justify-center rounded-md border border-neutral-400 border-opacity-30 bg-transparent px-4 py-2 font-semibold leading-none text-neutral-700 opacity-100 transition-all hover:border-opacity-50 hover:shadow-xs dark:border-neutral-700 dark:text-neutral-200 dark:hover:border-neutral-600 dark:hover:text-white sm:space-x-2"
+    className="shadow-2xs text-sm flex flex-none cursor-pointer items-center justify-center rounded-md border border-neutral-400/30 bg-transparent px-4 py-2 font-semibold leading-none text-neutral-700 opacity-100 transition-all hover:border-neutral-400/50 hover:shadow-xs dark:border-neutral-700 dark:text-neutral-200 dark:hover:border-neutral-600 dark:hover:text-white sm:space-x-2"
     target="_blank"
     rel="noopener noreferrer"
   >
