684c7e2052a397ab1f4a5c8afd0b695c51a9e8fd
Carlos Cuesta <carloscuesta@me.com>
2025-02-03 01:32:13 +0100

    🐛 Fix Tailwind opacity

--- src/app/about/components/Online/index.tsx
+++ src/app/about/components/Online/index.tsx
@@ -28,7 +28,7 @@ const Online = () => (
         <li key={profile.name}>
           <a
             href={profile.url}
-            className="hover:text-neutral-1000 shadow-2xs flex flex-none cursor-pointer items-center justify-center rounded-md border border-neutral-400/30 bg-transparent px-4 py-2 text-sm font-semibold leading-none text-neutral-700 opacity-100 transition-all hover:border-opacity-50 hover:shadow-xs dark:border-neutral-700/30 dark:text-neutral-200 dark:hover:border-neutral-600/30 dark:hover:text-white sm:space-x-2"
+            className="hover:text-neutral-1000 shadow-2xs flex flex-none cursor-pointer items-center justify-center rounded-md border border-neutral-400/30 bg-transparent px-4 py-2 text-sm font-semibold leading-none text-neutral-700 opacity-100 transition-all hover:border-neutral-400/50 hover:shadow-xs dark:border-neutral-700/30 dark:text-neutral-200 dark:hover:border-neutral-600/30 dark:hover:text-white sm:space-x-2"
             target="_blank"
             rel="noopener noreferrer"
           >
