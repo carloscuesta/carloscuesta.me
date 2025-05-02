d9f497a7d88a115ad59f92f8aa45c20d18ce27f3
Carlos Cuesta <carloscuesta@me.com>
2025-05-03 00:01:42 +0200

    ✨ Support `light` & `dark` only images in blog posts (#1622)

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,15 +1,25 @@
 import { MDXComponents } from 'mdx/types'
 import Image, { ImageProps } from 'next/image'
 import Playground from './app/blog/[slug]/components/Playground'
+import cn from 'src/utils/cn'
 
 const components = {
-  img: (props) => (
-    <Image
-      sizes="100vw"
-      style={{ width: '100%', height: 'auto' }}
-      {...(props as ImageProps)}
-    />
-  ),
+  img: (props) => {
+    const isDarkModeOnly = props.src.includes('#dark-mode-only')
+    const isLightModeOnly = props.src.includes('#light-mode-only')
+
+    return (
+      <Image
+        sizes="100vw"
+        style={{ width: '100%', height: 'auto' }}
+        className={cn(
+          isDarkModeOnly && 'hidden dark:block',
+          isLightModeOnly && 'block dark:hidden',
+        )}
+        {...(props as ImageProps)}
+      />
+    )
+  },
   Playground,
 } satisfies MDXComponents
 
