a4a3d1865ab6ffae9e56f8bdd7098b960cf81cb9
Carlos Cuesta <carloscuesta@me.com>
2026-07-12 00:48:45 +0200

    🐛 Fix TypeScript 7 + jest-fetch-mock v4 compatibility
    - Use npm:@typescript/typescript6 alias as typescript package to provide
      TS6 API for typescript-eslint while TypeScript 7.1 API ships
    - Update tscheck script to use tsc6 binary from @typescript/typescript6
    - Move enableFetchMocks() to jest.setup.ts (required by jest-fetch-mock v4)

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,8 @@
 import '@testing-library/jest-dom'
+import { enableFetchMocks } from 'jest-fetch-mock'
 import structuredClone from '@ungap/structured-clone'
+
+enableFetchMocks()
 
 if (typeof global.structuredClone === 'undefined') {
   // @ts-expect-error: Polyfilling in jest-env
